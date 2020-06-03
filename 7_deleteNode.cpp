31c2dcc6171498ed859441cf244c37e0c1d1aee6
Gabriele Boncoraglio <65842059+gboncoraglio@users.noreply.github.com>
2020-06-02 17:06:02 -0700

    Create 7_deleteNode.cpp

---
+++ 7_deleteNode.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next ;
+    }
+};
+
+
+/*
+4,5,9,9], node = 1
+
+Output: [4,5,9]
+*/
